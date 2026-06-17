a87e07fabe6f4fd91b75a37fce408f94c4ad2aa8
Suno-14 <sunosharjkr14@gmail.com>
2026-06-17 14:04:05 +0530

    Update zenoh_pub.cpp

--- src/zenoh_pub.cpp
+++ src/zenoh_pub.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <thread>
 #include <chrono>
-#include <zenoh/api/config.hxx>
-#include <zenoh/api/session.hxx>
+// #include <zenoh/api/config.hxx>
+#include <zenoh.hxx>
 
 int main() {
     auto session = zenoh::Session::open(zenoh::Config::create_default());
